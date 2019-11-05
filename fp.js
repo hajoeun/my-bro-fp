66f81c0afbf7ee6c370900e47892502616c61ac3
joeunha <imjoeunha@gmail.com>
2019-11-05 17:48:08 +0900

    After first session

--- fp.js
+++ fp.js
@@ -1,41 +1,53 @@
 !function(G) {
   G._ = {};
 
-  _.each = (list, iter) => {
+  const curry = fn => b => a => fn(a, b);
+
+  _.each = curry((list, iter) => {
     for (let v of list) {
       iter(v);
     }
     return list;
-  };
+  });
 
-  _.map = (list, iter) => {
+  _.map = curry((list, mapper) => {
     let result = [];
-    _.each(list, (v) => {
-      result.push(iter(v))
-    });
-    return result;
-  };
-
-  _.filter = (list, predicate) => {
-    let result = [];
-    _.each(list, (v) => {
-      if (predicate(v)) result.push(v);
-    });
-    return result;
-  };
-
-  _.reject = (list, predicate) => (
-    _.filter(list, (v) => !predicate(v))
-  );
-
-  _.find = (list, predicate) => {
     for (let v of list) {
-      if (predicate(v)) return v;
+      result.push(mapper(v));
     }
+    return result;
+  });
+
+  _.filter = curry((list, predicate) => {
+    let result = [];
+    _.each(v => {
+      if (predicate(v)) result.push(v);
+    })(list);
+    return result;
+  });
+
+  const not = v => !v;
+  _.not = not;
+  _.identity = v => v;
+  _.always = v => () => v;
+
+  _.reject = curry((list, predicate) => (
+    _.filter(_.pipe(predicate, not))(list)
+  ));
+
+   _.reduce = (list, reducer, acc) => {
+    let _list = [...list], _acc = acc || _list.shift();
+    _.each(v => _acc = reducer(_acc, v))(list);
+    return _acc;
   };
 
-  _.reduce = (list, reducer, acc) => {
-    let _list = [...list], _acc = acc || _list.shift();
-    return _list.reduce(reducer, _acc);
-  }
+  _.pipe = (...fns) => v => _.reduce(fns, (v, f) => f(v), v);
+
+  _.tap = (...fns) => v => {
+    _.pipe(...fns)(v);
+    return v;
+  };
+
+  _.hi = _.tap(console.log);
+
 }(window);
