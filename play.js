7fc0a70e81c742ab6aded34c7c673e20f26e8b33
hajoeun <hajoeun90@gmail.com>
2021-03-11 09:52:51 +0900

    Update for tech talk

--- play.js
+++ play.js
@@ -1,14 +1,5 @@
-// flow, curry
-
 // const swapDisplayOrder = (index1, index2) => {
 //   const swaped = swapItems(editedData, index1, index2);
 //
 //   setEditedData(mapDisplayOrder(swaped));
 // };
-
-// flow, curry라는 함수의 개념을 익힌다.
-// flow, curry를 사용해서 위의 코드를 재구성해본다.
-// flow, curry 함수를 직접 만들어본다.
-
-// flow 쓰는 방법
-//    1. 변수 선언을 없앤다. 2. 함수 실행 순서대로 나열한다. 3. 인자를 넣어준다.
